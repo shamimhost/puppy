e5929f8b09a0b243d3cba62ade2ee3a043d443a3
shamimhost <shamimhosting@outlook.com>
2024-08-21 11:16:02 +0330

    2nd

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/shamimhost/puppy
+
+go 1.21.5
---
+++ puppy.go
@@ -0,0 +1,12 @@
+package puppy
+
+func Bark() string {
+
+	return "Woof!"
+}
+
+func Barks() string {
+
+	return "Woof! Woof! Woof!"
+
+}
